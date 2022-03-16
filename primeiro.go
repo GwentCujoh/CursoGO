ca367c06a42ebc59a678cdd8a5ec5447969ed581
Gwent <44040877+GwentCujoh@users.noreply.github.com>
2022-03-16 15:09:48 -0300

    Add files via upload

---
+++ primeiro.go
@@ -0,0 +1,19 @@
+// Programas executavéis iniciam pelo pacote main
+package main
+
+/*
+Os Códigos são organizados em pacotes
+e para você usá-los é necessário declarar um ou vários imports
+
+
+
+*/
+
+import "fmt"
+
+// A porta de entrada de um programa Go é a função main
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+
+}
